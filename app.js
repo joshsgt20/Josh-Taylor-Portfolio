25c9584673b98ea2f0fb8b4c0e37c554b04465a3
Josh Taylor <64259172+joshsgt20@users.noreply.github.com>
2021-03-29 15:37:02 +0100

    FInal changes to portfolio

--- app.js
+++ app.js
@@ -64,6 +64,20 @@ const Welcome = document.querySelector('.Welcome');
 const Hamburger = document.querySelector('.Hamburger');
 const Intro = document.querySelector('.Intro_Text');
 
+const MoodsAnimation = document.querySelector('#MoodsAnimation');
+const LinksAnimation = document.querySelector('#LinksAnimation');
+const AdharAnimation = document.querySelector('#AdharAnimation');
+const UMHAnimation = document.querySelector('#UMHAnimation');
+
+const ContactPhoto = document.querySelector('#ContactPhoto');
+const ContactSocials = document.querySelector('#ContactSocials');
+
+const BlogPost1 = document.querySelector('#BlogPost1');
+const BlogPost2 = document.querySelector('#BlogPost2');
+const BlogPost3 = document.querySelector('#BlogPost3');
+const BlogPost4 = document.querySelector('#BlogPost4');
+
+
 
 const tl = new TimelineMax();
 
@@ -73,5 +87,18 @@ tl.fromTo(Slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut })
 .fromTo(WelcomeLinks, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0")
 .fromTo(Logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=2")
 .fromTo(Nav, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=2.5")
-.fromTo(Hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=2")
+.fromTo(Hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=2");
 
+
+tl.fromTo(MoodsAnimation, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-2.5")
+tl.fromTo(LinksAnimation, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-2.3")
+tl.fromTo(AdharAnimation, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-2.1")
+tl.fromTo(UMHAnimation, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-1.9")
+
+tl.fromTo(ContactPhoto, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-2")
+tl.fromTo(ContactSocials, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0, ease: Power1.easeInOut}, "=-2.5")
+
+tl.fromTo(BlogPost1, 1, {opacity: 0, y: 30}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "=-2.5")
+tl.fromTo(BlogPost2, 1, {opacity: 0, y: 30}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "=-2.3")
+tl.fromTo(BlogPost3, 1, {opacity: 0, y: 30}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "=-2.1")
+tl.fromTo(BlogPost4, 1, {opacity: 0, y: 30}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "=-1.9")
